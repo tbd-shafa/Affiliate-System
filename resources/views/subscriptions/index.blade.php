48c2a19c6f3020cddec89009af9f730ebca8c19c
tbd-shafa <shafa@technobd.com>
2024-12-30 18:17:32 +0600

    update user end

--- resources/views/subscriptions/index.blade.php
+++ resources/views/subscriptions/index.blade.php
@@ -1,32 +1,35 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            create Subscription
-        </h2>
-    </x-slot>
+
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6">
-                <div class="overflow-x-auto">
-                    @if (session('success'))
-                        <div x-data="{ open: true }" x-show="open" x-transition
-                            class="alert alert-success mb-4 bg-green-100 border-l-4 border-green-500 text-green-700 p-4">
-                            <div class="flex items-center justify-between">
-                                <div class="flex">
+                @if (session('success'))
+                    <div x-data="{ open: true }" x-show="open" x-transition
+                        class="alert alert-success mb-4 bg-green-100 border-l-4 border-green-500 text-green-700 p-4">
+                        <div class="flex items-center justify-between">
+                            <div class="flex">
 
-                                    <span>{{ session('success') }}</span>
-                                </div>
-                                <button @click="open = false" class="ml-4 text-gray-500 hover:text-gray-700">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none"
-                                        viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                            d="M6 18L18 6M6 6l12 12" />
-                                    </svg>
-                                </button>
+                                <span>{{ session('success') }}</span>
                             </div>
+                            <button @click="open = false" class="ml-4 text-gray-500 hover:text-gray-700">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none"
+                                    viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                        d="M6 18L18 6M6 6l12 12" />
+                                </svg>
+                            </button>
                         </div>
-                    @endif
-                     <form method="POST" action="{{ route('subscriptions.buy') }}">
+                    </div>
+                @endif
+                <div class="overflow-x-auto">
+
+                    <div class="flex items-center justify-between " style="padding-top:10px; padding-bottom:10px;">
+                        <h2 class="font-bold text-xl text-gray-800 leading-tight">
+                            Create Subscription
+                        </h2>
+
+                    </div>
+                    <form method="POST" action="{{ route('subscriptions.buy') }}">
                         @csrf
 
                         <!-- Subscription Plan Dropdown -->
