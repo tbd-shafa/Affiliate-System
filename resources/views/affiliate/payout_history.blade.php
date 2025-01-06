ead757dfadf40bd2327a29f34145a95aafdd4111
tbd-shafa <shafa@technobd.com>
2025-01-06 13:23:21 +0600

    payout history update

--- resources/views/affiliate/payout_history.blade.php
+++ resources/views/affiliate/payout_history.blade.php
@@ -24,7 +24,8 @@
                                 Payout Amount</th>
                             <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                 Remarks</th>
-                         
+                         <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                Payment By</th>
 
                         </tr>
                     </thead>
@@ -41,6 +42,9 @@
                                 </td>
                                 <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
                                     {{ $history->remarks }}
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                                    {{ $history->payment_by }}
                                 </td>
                                 
                             </tr>
